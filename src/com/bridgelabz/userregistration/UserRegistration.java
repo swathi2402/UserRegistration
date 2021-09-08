09eb8b6884dd5b90692d79a7d692690f3d775ddf
Swathi <swathi.h@perfios.com>
2021-09-08 07:30:31 +0530

    MODIFIED: Extended to validate last name

--- src/com/bridgelabz/userregistration/UserRegistration.java
+++ src/com/bridgelabz/userregistration/UserRegistration.java
@@ -5,22 +5,29 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
-	private static boolean userRegistration(String firstName) {
+	private static boolean validateName(String name) {
 		String regex = "([A-Z][a-zA-Z]{2,})";
 		Pattern pattern = Pattern.compile(regex);
-		Matcher matcher = pattern.matcher(firstName);
+		Matcher matcher = pattern.matcher(name);
 		return matcher.matches();
 		
 	}
-
+	
 	public static void main(String[] args) {
 		Scanner scanner=new Scanner(System.in);
 		System.out.println("Enter the first name: ");
 		String firstName = scanner.nextLine();
-		if(userRegistration(firstName))
-			System.out.println("Valid name");
+		if(validateName(firstName))
+			System.out.println("Valid first name");
 		else
-			System.out.println("Invalid name");
+			System.out.println("Invalid first name");
+		
+		System.out.println("Enter the last name: ");
+		String LastName = scanner.nextLine();
+		if(validateName(LastName))
+			System.out.println("Valid last name");
+		else
+			System.out.println("Invalid last name");
 		scanner.close();		
 	}
 
